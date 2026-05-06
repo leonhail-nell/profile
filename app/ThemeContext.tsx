d4976e3497e78c9405e2ac146b31759622eb3190
leonhail paypa <leopaypa52@gmail.com>
2026-05-06 03:51:20 +0000

    chore: remove profile website files from TaraFlight repo

--- app/ThemeContext.tsx
+++
@@ -1,9 +0,0 @@
-"use client";
-import { createContext, useContext } from "react";
-
-export const ColorModeContext = createContext({
-  mode: "light" as "light" | "dark",
-  toggleColorMode: () => {},
-});
-
-export const useColorMode = () => useContext(ColorModeContext);
